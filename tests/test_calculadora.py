c539288197fcdf21031b282b432c40ad861c6525
rdelaragomes <rdelaragomes@gmail.com>
2026-01-08 22:03:29 -0300

    Update test_calculadora.py

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,7 +1,6 @@
-# tests/test_calculadora.py  
-from src.calculadora import soma  
-def test_soma():  
-    assert soma(2, 3) == 5  
-    assert soma(-1, 1) == 0  
-    assert soma(0, 0) == 0  
+from src.calculadora import soma
 
+def test_soma():
+    assert soma(2, 3) == 5
+    assert soma(-1, 1) == 0
+    assert soma(0, 0) == 0
